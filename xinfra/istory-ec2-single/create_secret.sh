4bc3447b506080b03b9fa3ea60642bf53ca37fb5
been622 <sb6228@gmail.com>
2025-03-22 15:58:17 +0900

    add wf

--- xinfra/istory-ec2-single/create_secret.sh
+++ xinfra/istory-ec2-single/create_secret.sh
@@ -1,6 +1,7 @@
 # secret for github action
 gh api -X PUT repos/been622/istory-app/environments/dev --silent
 gh secret set MYSQL_DATABASE --env dev --body "istory"
+gh secret set AWS_S3_BUCKET --env dev --body "istory-deploy-bucket-314146313665"
 gh secret set DATABASE_URL --env dev --body "jdbc:mysql://istory-db.c700kuqmksj1.ap-northeast-2.rds.amazonaws.com:3306/istory"
 gh secret set MYSQL_USER --env dev --body "istory"
 gh secret set MYSQL_PASSWORD --env dev --body "user12345"
