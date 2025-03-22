993e335a7820a9666aaa4cb8d270cc1b01a4719d
been622 <sb6228@gmail.com>
2025-03-22 15:47:40 +0900

    ADD WORKFLOWS

---
+++ xinfra/istory-ec2-single/create_secret.sh
@@ -0,0 +1,10 @@
+# secret for github action
+gh api -X PUT repos/been622/istory-app/environments/dev --silent
+gh secret set MYSQL_DATABASE --env dev --body "istory"
+gh secret set DATABASE_URL --env dev --body "jdbc:mysql://istory-db.c700kuqmksj1.ap-northeast-2.rds.amazonaws.com:3306/istory"
+gh secret set MYSQL_USER --env dev --body "istory"
+gh secret set MYSQL_PASSWORD --env dev --body "user12345"
+gh secret set MYSQL_ROOT_PASSWORD --env dev --body "admin12345"
+gh secret set AWS_ACCESS_KEY_ID --env dev --body "AKIAUSJEUKXARDATMEDE"
+gh secret set AWS_SECRET_ACCESS_KEY --env dev --body "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"
+gh variable set AWS_REGION --env dev --body "ap-northeast-2"
